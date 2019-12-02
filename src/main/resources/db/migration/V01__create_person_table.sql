59d462af44a5e90c6a6e0269c7fcb45217d28fd3
Esiquiel Neto <ziki.neto@gmail.com>
2019-12-02 15:57:19 -0300

    criado os arquivos para verisonamento de banco das tabelas person a address

--- src/main/resources/db/migration/V01__create_person_table.sql
+++ src/main/resources/db/migration/V01__create_person_table.sql
@@ -1,3 +1,7 @@
-create table person {
+create table person (
+    id        BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+    name      VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    birth     DATETIME NOT NULL
 
-}
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- src/main/resources/db/migration/V02__create_address_table.sql
+++ src/main/resources/db/migration/V02__create_address_table.sql
@@ -1,3 +1,8 @@
-create table address {
-
-}
+create table address (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    city VARCHAR(255) NOT NULL,
+    district VARCHAR(255) NOT NULL,
+    street VARCHAR(255) NOT NULL,
+    number INT NOT NULL,
+    complement VARCHAR(255)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
